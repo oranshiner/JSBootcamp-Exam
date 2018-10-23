45a039efece7e13eefc807ef72c8072e4428e03d
oranshiner <42638110+oranshiner@users.noreply.github.com>
2018-10-23 22:02:21 +0300

    Update search.js

--- search.js
+++ search.js
@@ -1,48 +1,47 @@
+
+//############################
+//### Aurthor: Oran Shiner ###
+//###     23/10/2018       ###
+//############################ 
+
 var fs = require('fs');
 var path = require('path');
 var ext = process.argv[2];
 var text = process.argv[3];
+var flag = '0';
 
 //Checks if the user entered the variables
 if (ext == undefined || text == undefined) {
     console.log('USAGE: node search [EXT] [TEXT]');
 } else {
-    // Return a list of files
-    // with the file path absulot to the given dir
+    // This function find all the files with the relevant extention.
     function getFilesFromDir(dir, fileTypes, name) {
         var filesToReturn = [];
+        // This (recorsive) function is "walking" trow the folders.
         function walkDir(currentPath) {
             var files = fs.readdirSync(currentPath);
             for (var i in files) {
                 var currentFile = path.join(currentPath, files[i]);
-                //Splits the file name from the directory
-                var splitDirAndFileName = currentFile.split('\\');
-                //Take the last argument from the list - this is the name of the file dot external
-                var fullFileName = splitDirAndFileName[splitDirAndFileName.length - 1];
-                //Splits the Dot external from the file name
-                var splitRmDotExt = fullFileName.split('.');
-                //Check that this is the correct file
-                if (fs.statSync(currentFile).isFile() && fileTypes.indexOf(path.extname(currentFile)) != -1 && name === splitRmDotExt[0]) {
-                    filesToReturn.push(currentFile.replace(dir, ''));
-                //check if this is the last folder and goes back with recursion
+                // Checks if this is a file with the relevate extenction.
+                if (fs.statSync(currentFile).isFile() && fileTypes.indexOf(path.extname(currentFile)) != -1) {
+                    var data = fs.readFileSync(currentFile)
+                    if (data.indexOf(name) >= 0) {
+                            //Prints the Current File that was found.
+                            console.log(process.cwd() + '\\' + currentFile);
+                            filesToReturn.push(currentFile.replace(dir, ''));
+                            flag = '1';
+                        };
                 } else if (fs.statSync(currentFile).isDirectory()) {
                     walkDir(currentFile);
-                }
+                } 
             }
         };
         walkDir(dir);
-        return filesToReturn;
     }
-    var file_name = getFilesFromDir("./", ["." + ext], text);
-    //Print all results
-    for (var j in file_name) {
-        console.log(process.cwd() + '\\' + file_name[j]);
-    }
-    //Check if the file exists
-    if (file_name[j] == undefined) {
+
+    getFilesFromDir("./", ["." + ext], text);
+    // Checks the flag - '0' means that no file was found. '1' means the file was found.
+    if (flag === '0') {
         console.log("No file was found");
     }
 }
-
-//### Aurthor Oran Shiner ###
-//###    16/10/2018       ###
