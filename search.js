2f0f576d5b7b43679787f61b5323af09211cd41b
oranshiner <42638110+oranshiner@users.noreply.github.com>
2018-10-23 22:11:31 +0300

    Update search.js

--- search.js
+++ search.js
@@ -1,6 +1,6 @@
 
 //############################
-//### Aurthor: Oran Shiner ###
+//### Author: Oran Shiner  ###
 //###     23/10/2018       ###
 //############################ 
 
@@ -10,23 +10,23 @@ var ext = process.argv[2];
 var text = process.argv[3];
 var flag = '0';
 
-//Checks if the user entered the variables
+// Checks if the user entered the variables.
 if (ext == undefined || text == undefined) {
     console.log('USAGE: node search [EXT] [TEXT]');
 } else {
-    // This function find all the files with the relevant extention.
+    // This function finds all the files with the relevant extension.
     function getFilesFromDir(dir, fileTypes, name) {
         var filesToReturn = [];
-        // This (recorsive) function is "walking" trow the folders.
+        // This (recursive) function is "walking" through the folders.
         function walkDir(currentPath) {
             var files = fs.readdirSync(currentPath);
             for (var i in files) {
                 var currentFile = path.join(currentPath, files[i]);
-                // Checks if this is a file with the relevate extenction.
+                // Checks if this is a file with the relevant extension.
                 if (fs.statSync(currentFile).isFile() && fileTypes.indexOf(path.extname(currentFile)) != -1) {
                     var data = fs.readFileSync(currentFile)
                     if (data.indexOf(name) >= 0) {
-                            //Prints the Current File that was found.
+                            // Prints the current file found.
                             console.log(process.cwd() + '\\' + currentFile);
                             filesToReturn.push(currentFile.replace(dir, ''));
                             flag = '1';
@@ -40,7 +40,7 @@ if (ext == undefined || text == undefined) {
     }
 
     getFilesFromDir("./", ["." + ext], text);
-    // Checks the flag - '0' means that no file was found. '1' means the file was found.
+    // Checks the flag - '0' means that no file was found, '1' means the file was found.
     if (flag === '0') {
         console.log("No file was found");
     }
